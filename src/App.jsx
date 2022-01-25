899df9634077b2660b47c670664d58c414c0ea1d
Tozarf <fazatojuni@gmail.com>
2022-01-24 21:48:54 -0600

    Routes added

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Layout } from "./layout/Layout";
-import { Login } from "./layout/Login";
+
 import { HomePage } from "./pages/HomePage";
-import { LoginForm } from "./pages/LoginForm";
+
+import { NewClient } from "./pages/NewClient";
+import { EditClient } from "./pages/EditClient";
 
 function App() {
     return (
         <Router>
             <Routes>
                 <Route>
-                    <Route path="/" element={<Login />}>
-                        <Route index element={<LoginForm />} />
-                    </Route>
                     <Route path="/clients" element={<Layout />}>
                         <Route index element={<HomePage />} />
+                        <Route path="new" element={<NewClient />} />
+                        <Route path="edit/:id" element={<EditClient />} />
                     </Route>
                 </Route>
             </Routes>
--- src/pages/NewClient.jsx
+++ src/pages/NewClient.jsx
@@ -1,11 +1,9 @@
 import React from "react";
 
-const NewClient = () => {
+export const NewClient = () => {
     return (
         <div>
             <h1>NewClient</h1>
         </div>
     );
 };
-
-export default NewClient;
